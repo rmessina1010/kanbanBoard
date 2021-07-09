ddf569a6587851f1184c022633a2e8e355d17c4f
Ray Messina <rmessina10@gmail.com>
2021-07-09 16:18:11 -0500

    Sample data; react-beautiful-dnd

---
+++ src/components/data.js
@@ -0,0 +1,85 @@
+export const projects = [
+    {
+        id: 1,
+        title: 'project1',
+        open: true,
+        steps: [
+            {
+                step: 'one',
+                stepId: 1,
+                items: [{
+                    id: 1,
+                    item: 'item1',
+                    desc: 'bla blah',
+                    img: null,
+                    status: false,
+                },
+                {
+                    id: 2,
+                    item: 'item2',
+                    desc: 'bla blah',
+                    img: null,
+                    status: false,
+                },
+                {
+                    id: 3,
+                    item: 'item3',
+                    desc: 'Lorem Ipsum',
+                    img: null,
+                    status: false,
+                },
+                {
+                    id: 4,
+                    item: 'item4',
+                    desc: 'code improvement',
+                    img: null,
+                    status: false,
+                },
+                {
+                    id: 5,
+                    item: 'item5',
+                    desc: 'bla blah',
+                    img: null,
+                    status: false,
+                },
+                {
+                    id: 6,
+                    item: 'item6',
+                    desc: 'bla blah',
+                    img: null,
+                    status: false,
+                }]
+            },
+
+            {
+                step: 'two',
+                stepId: 2,
+                items: []
+            },
+            {
+                step: 'three',
+                stepId: 3,
+                items: []
+            },
+            {
+                step: 'four',
+                stepId: 4,
+                items: []
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: 'project2',
+        open: true,
+        steps: [
+            {
+                step: 'one',
+                stepId: 1,
+                items: []
+            }
+
+        ]
+    }
+
+]
